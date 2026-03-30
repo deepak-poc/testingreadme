4f46ef2f4417e888c9101f509351255144b5ee36
deepak-poc <deepakmeeenakshi@gmail.com>
2026-03-30 14:56:34 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -38,3 +38,34 @@ resource "azurerm_service_plan" "main" {
   tags                = var.tags
 }
 
+
+resource "azurerm_linux_function_app" "function_app" {
+  name                       = var.function_app_name
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  service_plan_id            = azurerm_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.function_sa.name
+  storage_account_access_key = azurerm_storage_account.function_sa.primary_access_key
+  virtual_network_subnet_id  = azurerm_subnet.function_subnet.id
+
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+
+  app_settings = {
+    # Blob trigger source: primary-sa / file-ingest
+    PRIMARY_SA_CONNECTION_STRING = azurerm_storage_account.primary_sa.primary_connection_string
+
+    # Destination: scanner-sa / scanner
+    SCANNER_SA_CONNECTION_STRING = azurerm_storage_account.scanner_sa.primary_connection_string
+
+    FUNCTIONS_WORKER_RUNTIME = "python"
+    AzureWebJobsStorage      = azurerm_storage_account.function_sa.primary_connection_string
+
+    # Oryx installs packages server-side during zip deploy
+    SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
+    ENABLE_ORYX_BUILD              = "true"
+  }
+}
